1c6dbf7926ec301f44395df331d10f491e46958d
hristiyanaSh <131446919+hristiyanaSh@users.noreply.github.com>
2023-06-02 14:39:33 +0300

    First steps into my first project

---
+++ src/test/java/com/example/MyFirstSpringBootProject/MyFirstSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
